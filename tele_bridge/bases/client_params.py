c3671409c0f84799673323c2104fe2faffc62455
taimast <charlskenno@gmail.com>
2024-08-24 23:28:24 +0300

    Refactor client_params to use Autofill mixin for improved code reuse and maintainability

--- tele_bridge/bases/client_params.py
+++ tele_bridge/bases/client_params.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+from tele_bridge.bases.mixins import Autofill
 from tele_bridge.sessions.tele_bridge_session import TeleBridgeSession
 
 
@@ -13,12 +14,12 @@ class ClientOpts:
     receive_updates: bool = True
 
     phone_number: str = None
-    phone_code: str = None
-    password: str = None
+    phone_code: Autofill = None
+    password: Autofill = None
 
-    phone_number_error: str = None
-    phone_code_error: str = None
-    password_error: str = None
+    phone_number_error: Autofill = None
+    phone_code_error: Autofill = None
+    password_error: Autofill = None
     proxy: str = None
 
     # seconds
